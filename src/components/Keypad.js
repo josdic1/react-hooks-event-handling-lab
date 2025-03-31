32f3d00de94b475c4f56c25581a9c37fc6298232
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-03-31 09:53:20 -0400

    event handling lab done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,23 @@
 // Code Keypad Component Here
 
+
+
 function Keypad (){
+
+let password;
+
+function onType(pass) {
+    password = pass
+       console.log('Entering password...')
+       return password
+}
+
     return (
-        <div></div>
+        <>
+        <div>
+            <input type='password' value={password} onChange={(e) => onType(e.target.value)} />
+        </div>
+        </>
     )
 }
 
